dcabaf4d172c977099ffcb0cab49eb3d3a2feb1b
Auxietre <froxworld@gmail.com>
2019-01-09 12:18:17 +0100

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+import { AppComponent }         from './app.component';
+import { ResumeComponent }   from './resume/resume.component';
+import { ModelDetailComponent }  from './model-detail/model-detail.component';
+import { ModelsComponent }      from './models/models.component';
+import { ModelSearchComponent }  from './model-search/model-search.component';
+import { MessagesComponent }    from './messages/messages.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    ResumeComponent,
+    ModelsComponent,
+    ModelDetailComponent,
+    MessagesComponent,
+    ModelSearchComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
